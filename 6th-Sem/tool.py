7f60faa53b27ec20f911fc3ba7939b2c3f091a6d
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2022-07-12 12:05:20 +0530

    Update tool.py

--- 6th-Sem/tool.py
+++ 6th-Sem/tool.py
@@ -156,9 +156,6 @@ class Spinner:
     @staticmethod
     def spinning_cursor():
         while 1:
-            #for cursor in '|/-\\/': yield cursor #←↑↓→
-            #for cursor in '←↑↓→': yield cursor
-            #for cursor in '....scanning...please..wait....': yield cursor
             for cursor in ' ': yield cursor
     def __init__(self, delay=None):
         self.spinner_generator = self.spinning_cursor()
